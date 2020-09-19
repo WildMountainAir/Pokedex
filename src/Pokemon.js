32acc26df73a5ddda586f8c2dd543a4af8e06591
Alison Sipos <alison.sipos@icloud.com>
2020-09-19 15:20:17 -0600

    Created template to display single pokemon.

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// single pokémon
+// class: pokemon
+// image source and pokémon name will come from props
 function Pokemon(props) { 
   return (
     <div className="pokemon">
-      Single Pokémon
+      <img src={props.img}/>
+      <h2>{props.name}</h2>
     </div>
   );
 }
