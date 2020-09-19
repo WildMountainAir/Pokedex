9078ca005c5f033412aa04721f72c9b4bdd42383
Alison Sipos <alison.sipos@icloud.com>
2020-09-19 15:20:48 -0600

    Created template to display all pokemon in pokedex.

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Pokemon from './Pokemon';
 
+// all pokémon - props
+// class: pokemon-list
+// components: Pokemon
+// map over pokedex passed down from props
+// pass key and pokémon to Pokemon
 function PokemonList(props) {
   return (
     <div className='pokemon-list'>
-      Pokémon List
+      <h2>My Pokémon</h2>
+      {props.pokedex.map((pokemon, index) => <Pokemon key={index} {...pokemon}/>)}
     </div>
   );
 }
